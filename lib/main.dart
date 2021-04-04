f5cb01e453131fd59f275c9347fa112032d0e868
Missing-Tech <josephgrabski31@gmail.com>
2021-04-04 15:08:45 +0100

    Refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'auth.dart';
+import 'package:notes_app/screens/sign_in_screen.dart';
 
-FirebaseAnalytics analytics;
+FirebaseAnalytics? analytics;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,69 +17,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'QuoteNote',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('QuoteNote'),
-              backgroundColor: Colors.teal,
-            ),
-            body: Center(
-              child: Column(
-                children: <Widget>[
-                  LoginButton(), // <-- Built with StreamBuilder
-                  UserProfile() // <-- Built with StatefulWidget
-                ],
-              ),
-            )));
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          brightness: Brightness.dark,
+        ),
+        home: SignInScreen(),
+    );
   }
 }
 
-class UserProfile extends StatefulWidget {
-  @override
-  _UserProfileState createState() => _UserProfileState();
-}
-
-class _UserProfileState extends State<UserProfile> {
-  Map<String, dynamic> _profile;
-  bool _loading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    authService.profile.listen((state) => setState(() => _profile = state));
-
-    authService.loading.listen((state) => setState(() => _loading = state));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
-      Text(_loading.toString())
-    ]);
-  }
-}
-
-class LoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: authService.user,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MaterialButton(
-              onPressed: () => authService.signOut(),
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text('Signout'),
-            );
-          } else {
-            return MaterialButton(
-              onPressed: () => authService.googleSignIn(),
-              color: Colors.white,
-              textColor: Colors.black,
-              child: Text('Login with Google'),
-            );
-          }
-        });
-  }
-}
