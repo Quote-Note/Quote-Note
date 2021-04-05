372517b6e94efeb310bcfe6895e477a5d8dfbf4e
Missing-Tech <josephgrabski31@gmail.com>
2021-04-05 14:39:51 +0100

    Fixed issues with long names/emails

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -75,7 +75,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
           elevation: 0,
           backgroundColor: CustomColors.white,
           title: AppBarTitle(
-              photoUrl: _user.photoURL!, name: _user.displayName!, email: _user.email!,),
+              photoUrl: _user.photoURL, name: _user.displayName, email: _user.email!,),
         ),
       ),
       body: SafeArea(
--- lib/widgets/app_bar_title.dart
+++ lib/widgets/app_bar_title.dart
@@ -5,11 +5,12 @@ import 'package:notes_app/screens/sign_in_screen.dart';
 import 'package:notes_app/utils/auth.dart';
 
 class AppBarTitle extends StatefulWidget {
-  final String photoUrl;
-  final String name;
-  final String email;
+  final String? photoUrl;
+  final String? name;
+  final String? email;
 
-  const AppBarTitle({this.photoUrl = '', this.name = '', required this.email});
+  const AppBarTitle(
+      {this.photoUrl = null, this.name = null, required this.email});
 
   @override
   _AppBarTitleState createState() => _AppBarTitleState();
@@ -45,88 +46,92 @@ class _AppBarTitleState extends State<AppBarTitle> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Neumorphic(
-            style: NeumorphicStyle(
-              depth: -1,
-              intensity: 1,
-              boxShape: NeumorphicBoxShape.circle(),
-            ),
-            padding: const EdgeInsets.all(5),
-            child: ClipOval(
-              child: widget.photoUrl != ''
-                  ? Image.network(
-                      widget.photoUrl,
-                      scale: 2.2,
-                      fit: BoxFit.fitHeight,
-                    )
-                  : Icon(Icons.person_outlined, color: CustomColors.darkGrey),
+    return Row(
+      children: [
+        Neumorphic(
+          style: NeumorphicStyle(
+            depth: -1,
+            intensity: 1,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          padding: const EdgeInsets.all(5),
+          child: ClipOval(
+            child: widget.photoUrl != null
+                ? Image.network(
+                    widget.photoUrl.toString(),
+                    scale: 2.2,
+                    fit: BoxFit.fitHeight,
+                  )
+                : Icon(Icons.person_outlined, color: CustomColors.darkGrey),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+                widget.name != ''
+                  ? widget.name.toString()
+                  : widget.email.toString().substring(0, widget.email.toString().indexOf('@')),
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  color: CustomColors.darkGrey,
+                )),
+          ),
+        ),
+        SizedBox(width: 10,),
+        NeumorphicButton(
+          onPressed: toggleAdmin,
+          style: NeumorphicStyle(
+            depth: 3,
+            intensity: 1,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          padding: const EdgeInsets.all(5),
+          child: ClipOval(
+            child: isAdmin
+                ? Icon(Icons.star, color: CustomColors.yellow)
+                : Icon(Icons.star_border_outlined,
+                    color: CustomColors.darkGrey),
+          ),
+        ),
+        SizedBox(width: 10,),
+        NeumorphicButton(
+          onPressed: () async {},
+          style: NeumorphicStyle(
+            depth: 3,
+            intensity: 1,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          padding: const EdgeInsets.all(5),
+          child: ClipOval(
+            child: Icon(
+              Icons.edit,
+              color: CustomColors.primary,
             ),
           ),
-          Text(
-              widget.name != ''
-                  ? widget.name
-                  : widget.email.substring(0, widget.email.indexOf('@')),
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color: CustomColors.darkGrey,
-              )),
-          NeumorphicButton(
-            onPressed: toggleAdmin,
-            style: NeumorphicStyle(
-              depth: 3,
-              intensity: 1,
-              boxShape: NeumorphicBoxShape.circle(),
-            ),
-            padding: const EdgeInsets.all(5),
-            child: ClipOval(
-              child: isAdmin
-                  ? Icon(Icons.star, color: CustomColors.yellow)
-                  : Icon(Icons.star_border_outlined,
-                      color: CustomColors.darkGrey),
+        ),
+        SizedBox(width: 10,),
+        NeumorphicButton(
+          onPressed: () async {
+            setState(() {});
+            await Authentication.signOut(context: context);
+            setState(() {});
+            Navigator.of(context).pushReplacement(_routeToSignInScreen());
+          },
+          style: NeumorphicStyle(
+            depth: 3,
+            intensity: 1,
+            boxShape: NeumorphicBoxShape.circle(),
+          ),
+          padding: const EdgeInsets.all(5),
+          child: ClipOval(
+            child: Icon(
+              Icons.logout,
+              color: CustomColors.primary,
             ),
           ),
-          NeumorphicButton(
-            onPressed: () async {
-            },
-            style: NeumorphicStyle(
-              depth: 3,
-              intensity: 1,
-              boxShape: NeumorphicBoxShape.circle(),
-            ),
-            padding: const EdgeInsets.all(5),
-            child: ClipOval(
-              child: Icon(
-                Icons.edit,
-                color: CustomColors.primary,
-              ),
-            ),
-          ),
-          NeumorphicButton(
-            onPressed: () async {
-              setState(() {});
-              await Authentication.signOut(context: context);
-              setState(() {});
-              Navigator.of(context).pushReplacement(_routeToSignInScreen());
-            },
-            style: NeumorphicStyle(
-              depth: 3,
-              intensity: 1,
-              boxShape: NeumorphicBoxShape.circle(),
-            ),
-            padding: const EdgeInsets.all(5),
-            child: ClipOval(
-              child: Icon(
-                Icons.logout,
-                color: CustomColors.primary,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
