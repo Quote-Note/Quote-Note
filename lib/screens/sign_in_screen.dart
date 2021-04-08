991a7441e3a2060219a0a9cd6a5dcc4d71c30670
Missing-Tech <josephgrabski31@gmail.com>
2021-04-08 14:18:01 +0100

    Added animations to log in screen

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -18,15 +19,12 @@ class SignInScreen extends StatefulWidget {
 Route _routeToSignUpScreen() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => SignUpScreen(),
+    transitionDuration: Duration(milliseconds:500),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(1.0, 0.0);
-      var end = Offset.zero;
-      var curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
+      return SharedAxisTransition(
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        transitionType: SharedAxisTransitionType.horizontal,
         child: child,
       );
     },
